f08f53ef358897598a13dca938c9ac236f8fc851
Marcel Sawicki <msawicki@it-dev.pl>
2019-08-01 07:36:15 +0200

    Wywołąnie za pomocą delegacji różnych metod

---
+++ Delegat/Wywolanie-za-pomoca-delegacji-roznych-metod/Wywolanie-za-pomoca-delegacji-roznych-metod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wywolanie_za_pomoca_delegacji_roznych_metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
