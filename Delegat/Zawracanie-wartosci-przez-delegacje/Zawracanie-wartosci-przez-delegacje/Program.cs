765eb6843533ca03550cca9142a14af51d7d78af
Marcel Sawicki <msawicki@it-dev.pl>
2019-08-01 06:48:59 +0200

    Zwracanie wartości przy wywołaniu metody przez delegację

---
+++ Delegat/Zawracanie-wartosci-przez-delegacje/Zawracanie-wartosci-przez-delegacje/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zawracanie_wartosci_przez_delegacje
+{
+    class Program
+    {
+        public delegate int Delegacja(int arg1, int arg2);
+
+        public static int Dodaj(int argument1, int argument2)
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
